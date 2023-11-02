7c003633505a58ff250704c743f3c1576ea72a4c
Hoang Anh <hoanganh81097@gmail.com>
2023-11-02 23:44:19 +0700

    data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -23,11 +23,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql", 
-    host: process.env.DB_HOST.toString(),
+    host: process.env.DB_HOST,
     port: 3306,
-    username: process.env.DB_USERNAME.toString(),
-    password: process.env.DB_PASSWORD.toString(),
-    database: process.env.DB_DATABASE.toString(),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     entities: ["dist/src/entity/*.js"],
     extra: {
